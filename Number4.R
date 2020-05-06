2b35427516b2971ee096fe988965e8aa70c29121
Amanda Doyle <kryten4213@gmail.com>
2020-05-06 09:28:27 -0700

    Basic practice with dplyr

--- Number4.R
+++ Number4.R
@@ -1,3 +1,13 @@
+install.packages("tidyverse")
 library(tidyverse)
-
+library(dplyr)
 survey <- read.csv("surveys.csv")
+
+shrubery <- read.csv("shrub-volume-data.csv")
+names(shrubery)
+str(shrubery)
+select(shrubery,length)
+select(shrubery, site, experiment)
+filter(shrubery, height>5)
+shrub_data_w_vols <- mutate(shrubery, volume=length*width*height)
+shrub_data_w_vols

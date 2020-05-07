b78a0a6aac05c4e4db1cafc3ca1e1a67525ed1e6
Amanda Doyle <kryten4213@gmail.com>
2020-05-07 06:57:36 -0700

    Lecture 7 exercise 2

--- Lecture7.R
+++ Lecture7.R
@@ -6,3 +6,23 @@ pounds_to_grams <- function(pounds) {
 pounds_to_grams(3.75)   #Ans: 1700.97g
 
 #2.1
+get_mass_from_length_theropoda <- function(length){
+  mass <- 0.73 * length ^ 3.63
+  return(mass)
+}#estimates body mass in kg of a theropod based on its body length in meters
+#2.2
+get_mass_from_length_theropoda(16) #Spinosaurus
+#Ans: 17150.56 kg
+#2.3
+get_mass_from_length <- function(length, a, b) {
+  mass <- a * length ^b
+  return(mass)
+}#estimates body mass in kg based on body length (m) & allometric values a & b
+get_mass_from_length(26, 214.44, 1.46) #some sauropod
+#Ans: 24955.54 kg
+
+#3.1
+
+
+
+

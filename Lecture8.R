0e65fc4249daef8e7df310277ef5a828ac71b786
Amanda Doyle <kryten4213@gmail.com>
2020-05-07 07:32:54 -0700

    Lecture 8 exercise 1

---
+++ Lecture8.R
@@ -0,0 +1,24 @@
+w <- 10.2
+x <- 1.3
+y <- 2.8
+z <- 17.5
+colors <- c("red", "blue", "green")
+masses <- c(45.2, 36.1, 27.8, 81.6, 42.4)
+dna1 <- "attattaggaccaca"
+dna2 <- "attattaggaacaca"
+
+#1.1
+w>10
+#1.2
+"green" %in% colors
+#1.3
+x>y
+#1.4
+masses>40
+
+#2.1
+
+
+
+
+

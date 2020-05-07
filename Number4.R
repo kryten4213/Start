de637ce65e640cb85f733a78fe3df52a889b767b
Amanda Doyle <kryten4213@gmail.com>
2020-05-07 03:17:33 -0700

    Lecture 4 exercise 2

--- Number4.R
+++ Number4.R
@@ -12,3 +12,29 @@ filter(shrubery, height>5)
 shrub_data_w_vols <- mutate(shrubery, volume=length*width*height)
 shrub_data_w_vols
 
+#2.1
+survey_abr <- select(survey, year, month, day, species_id)
+#2.2
+survey_kg <- mutate(survey, weight_kg=weight/1000)
+survey_kg
+survey_kg <- select(survey_kg, year, species_id, na.omit(weight_kg))
+survey_kg <- na.omit(survey_kg)
+#2.3
+filter(survey, species_id=='SH')
+#2.4
+survey_byID <- group_by(survey, species_id)
+species_numIDs <- summarize(survey_byID, n())
+species_numIDs
+#2.5
+survey_by_ID_year <- group_by(survey, species_id, year)
+species_num_ID_year <- summarize(survey_by_ID_year, n())
+species_num_ID_year
+#2.6
+survey_DO <- filter(survey, species_id=='DO')
+survey_DO
+survey_DO_year <- group_by(survey_DO, year)
+survey_DO_weightByYear <- summarize(survey_DO_year, avg_weight=mean(weight, na.rm=TRUE))
+survey_DO_weightByYear
+
+
+

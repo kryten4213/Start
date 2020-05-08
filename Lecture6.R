b0aadf71a3a7237b94a19b146afa933c99417350
Amanda Doyle <kryten4213@gmail.com>
2020-05-08 04:55:11 -0700

    Lecture 6 exercise 3

--- Lecture6.R
+++ Lecture6.R
@@ -11,3 +11,21 @@ files <- dir()
 grep('.R$', files, value = TRUE)
 
 #3.1
+(strings <- c("^ab", "ab", "abc", "abd", "abe", "ab 12"))
+grep("ab.", strings, value = TRUE)
+grep("ab[c-e]", strings, value = TRUE)
+grep("ab[^c]", strings, value = TRUE)
+grep("^ab", strings, value = TRUE)
+grep("\\^ab", strings, value = TRUE)
+grep("abc|abd", strings, value = TRUE)
+gsub("(ab) 12", "\\1 34", strings)
+
+it_land <- grep(("[ITit]"), levels(gDat$country), value=TRUE)
+it_land <- grep(("land$"), it_land, value = TRUE)
+it_land <- gsub(("land"), ("\\1LAND"), it_land)
+it_land
+
+#4.1
+
+
+
